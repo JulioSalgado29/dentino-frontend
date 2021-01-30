b66af4e986c625950f144cb6355175b7b49a8142
Julio Salgado <juliosalgado2998@hotmail.com>
2021-01-29 21:51:45 -0500

    terminado

---
+++ src/Componentes/Usuario/Modulos/AgregarPacienteComponent.js
@@ -0,0 +1,286 @@
+import React from 'react';
+import UsuarioService from '../../../Servicios/UsuarioService';
+import Form from "react-validation/build/form";
+import Input from "react-validation/build/input";
+import CheckButton from "react-validation/build/button";
+import { isEmail, isEmpty, isNumeric } from 'validator';
+import HeaderComponent from '../../Complementos/HeaderComponent';
+import Sidebar from '../../Complementos/SidebarComponent';
+import Swal from 'sweetalert2';
+
+  const required = value => {
+    if (value.length===0) {
+      return (
+        <div className="alert alert-danger" role="alert">
+          Este campo es requerido
+        </div>
+      );
+    }
+  }; 
+  const email = value => {
+    if (!isEmail(value)) {
+      return (
+        <div className="alert alert-danger" role="alert">
+          El email no es valido
+        </div>
+      );
+    }
+  };
+  const password = value => {
+    if (value.length < 6 ) {
+      return (
+        <div className="alert alert-danger" role="alert">
+          La contraseña debe tener mas de 6 caracteres.
+        </div>
+      );
+    }
+  };
+
+class AgregarPacienteComponent extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            nombre: '',
+            apellido: '',
+            fechaNac: '',
+            email: '',
+            direccion: '',
+            telefono: '',
+            genero: '',
+            username: '',
+            password: '',
+            loading: false,
+            message: "",
+            isFocus: false,
+        }
+        this.ChangeNombreHandler = this.ChangeNombreHandler.bind(this);
+        this.ChangeApellidoHandler = this.ChangeApellidoHandler.bind(this);
+        this.ChangeFechaNacHandler = this.ChangeFechaNacHandler.bind(this);
+        this.ChangeEmailHandler = this.ChangeEmailHandler.bind(this);
+        this.ChangeDireccionHandler = this.ChangeDireccionHandler.bind(this);
+        this.ChangeTelefonoHandler = this.ChangeTelefonoHandler.bind(this);
+        this.ChangeGeneroHandler = this.ChangeGeneroHandler.bind(this);
+        this.ChangeUserNameHandler = this.ChangeUserNameHandler.bind(this);
+        this.ChangePasswordHandler = this.ChangePasswordHandler.bind(this);
+        this.saveUsuario = this.saveUsuario.bind(this);
+        this.cancel = this.cancel.bind(this);
+
+        this.onMouseEnter = this.onMouseEnter.bind(this);
+        this.onBlur	 = this.onBlur.bind(this);
+        this.onFocus = this.onFocus.bind(this);
+        this.onMouseOut = this.onMouseOut.bind(this);
+    }
+
+    componentDidMount() {
+      if (localStorage.getItem("isLandingPage")) {
+          localStorage.removeItem("isLandingPage");
+          window.location.reload();
+      }
+  }
+
+    saveUsuario(e){
+        e.preventDefault();
+        this.setState({
+            message: "",
+            loading: true,
+          });
+          if(this.form === undefined){
+            this.setState({
+              loading: false
+            });
+            Swal.fire
+            ({title: "Esta opción aun no se encuentra habilitada",
+              icon: 'warning',
+              backdrop: 'rgba(50, 50, 30, 0.5)'})
+          }
+          else{
+          this.form.validateAll();
+          if (this.checkBtn.context._errors.length === 0){
+            UsuarioService.registrar_user(/*this.state.nombre, this.state.apellido, this.state.fechaNac, this.state.email, this.state.direccion, this.state.telefono, 
+                                    this.state.genero, this.state.username, this.state.password*/)
+            .then(() => {
+              this.props.history.push("/");
+              //window.location.reload();
+            },
+            error => {
+              if(error.response.status === 401){
+                this.setState({
+                  loading: false,
+                  message: "Personal No autorizado"
+                });
+              }
+              else{
+              const resMessage =
+              (error.response && error.response.data) || error.message 
+              || error.toString();
+              this.setState({
+                loading: false,
+                message: resMessage
+              });
+            }})
+            .catch(function(error){
+              this.setState({
+                loading: false,
+                message: "Error de Conexión con el Servidor"
+              });
+            }.bind(this))
+          }else {
+            this.setState({
+              loading: false
+            });
+        }}
+    }
+    ChangeNombreHandler= (event) => {
+      this.setState({nombre: event.target.value})
+    }
+    ChangeApellidoHandler= (event) => {
+      this.setState({apellido: event.target.value})
+    }
+    ChangeFechaNacHandler= (event) => {
+      this.setState({fechaNac: event.target.value})
+    }
+    ChangeEmailHandler= (event) => {
+      this.setState({email: event.target.value})
+    }
+    ChangeDireccionHandler= (event) => {
+      this.setState({direccion: event.target.value})
+    }
+    ChangeTelefonoHandler= (event) => {
+      this.setState({telefono: event.target.value})
+    }
+    ChangeGeneroHandler= (event) => {
+      this.setState({genero: event.target.value})
+    }
+    ChangeUserNameHandler= (event) => {
+        this.setState({username: event.target.value})
+    }
+    ChangePasswordHandler= (event) => {
+        this.setState({password: event.target.value})
+    }
+    cancel(){
+        this.props.history.push('/')
+    }
+    onBlur(e) {
+      this.state.isFocus=false;
+      e.currentTarget.type = "text";
+    }
+    onFocus(e) {
+      this.state.isFocus=true;
+      e.currentTarget.type = "month";
+    }
+    onMouseEnter (e){
+      e.currentTarget.type = "month";
+    }
+    onMouseOut (e){
+      if(this.state.isFocus===false){
+        e.currentTarget.type = "text";
+      }
+    }
+    render (){
+        return(
+            <body class="hold-transition sidebar-mini layout-fixed">
+                <div class="wrapper">
+                    <HeaderComponent />
+                    <Sidebar />
+                    <div className="content-wrapper" style={{background:"white"}}>
+                        <div className="container-modulos">
+                            <div class="wrap-register200" style={{width:''}}>
+                            <span class="login100-form-title">
+                                        Registrar Paciente
+                                    </span>
+                                <form class="register100-form validate-form" style={{display:"contents"}}>
+                                <div class="container container-register">
+                                    <div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+                                        <input class="input100-julio" type="text" name="email" placeholder="Nombres" autofocus/>
+                                        <span class="focus-input100"></span>
+                                        <span class="symbol-input100">
+                                            <i class="fa fa-user" aria-hidden="true"></i>
+                                        </span>
+                                    </div>
+                                    <div class="wrap-input100 validate-input" data-validate = "Password is required">
+                                        <input class="input100-julio" type="text" name="pass" placeholder="Apellidos"/>
+                                        <span class="focus-input100"></span>
+                                        <span class="symbol-input100">
+                                            <i class="fa fa-user" aria-hidden="true"></i>
+                                        </span>
+                                    </div>
+                                </div>
+                                <div class="container container-register">
+                                    <div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+                                        <input class="input100-julio textbox-n" type="text" name="bday-month" placeholder="Fecha de Nacimiento"
+                                        onFocus={this.onFocus} onBlur={this.onBlur} onMouseEnter={this.onMouseEnter} onMouseOut={this.onMouseOut}/>
+                                        <span class="focus-input100"></span>
+                                        <span class="symbol-input100">
+                                            <i class="fa fa-calendar" aria-hidden="true"></i>
+                                        </span>
+                                    </div>
+                                    <div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+                                        <input class="input100-julio" type="text" name="email" placeholder="Email"/>
+                                        <span class="focus-input100"></span>
+                                        <span class="symbol-input100">
+                                            <i class="fa fa-envelope" aria-hidden="true"></i>
+                                        </span>
+                                    </div>
+                                </div>
+                                <div class="container container-register">
+                                <div class="wrap-input100 validate-input" data-validate = "Password is required">
+                                        <input class="input100-julio" type="text" name="pass" placeholder="Dirección"/>
+                                        <span class="focus-input100"></span>
+                                        <span class="symbol-input100">
+                                            <i class="fa fa-home" aria-hidden="true"></i>
+                                        </span>
+                                    </div>
+                                    <div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+                                        <input class="input100-julio" type="text" name="email" placeholder="Teléfono"/>
+                                        <span class="focus-input100"></span>
+                                        <span class="symbol-input100">
+                                            <i class="fa fa-phone" aria-hidden="true"></i>
+                                        </span>
+                                    </div>
+                                </div>
+                                <div class="container container-register">
+                                <div class="wrap-input100 validate-input" data-validate = "Password is required">
+                                        <input class="input100-julio" type="text" name="pass" placeholder="Género"/>
+                                        <span class="focus-input100"></span>
+                                        <span class="symbol-input100">
+                                            <i class="fa fa-transgender" aria-hidden="true"></i>
+                                        </span>
+                                    </div>
+                                    <div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+                                        <input class="input100-julio" type="text" name="email" placeholder="Username"/>
+                                        <span class="focus-input100"></span>
+                                        <span class="symbol-input100">
+                                            <i class="fa fa-user" aria-hidden="true"></i>
+                                        </span>
+                                    </div>
+                                </div>
+                                <div class="container container-register">
+                                <div class="wrap-input100 validate-input" data-validate = "Password is required">
+                                        <input class="input100-julio" type="password" name="pass" placeholder="Password"/>
+                                        <span class="focus-input100"></span>
+                                        <span class="symbol-input100">
+                                            <i class="fa fa-lock" aria-hidden="true"></i>
+                                        </span>
+                                    </div>
+                                    <div class="wrap-input100 validate-input" data-validate = "Password is required">
+                                        <input class="input100-julio" type="password" name="pass" placeholder="Confirmar Password"/>
+                                        <span class="focus-input100"></span>
+                                        <span class="symbol-input100">
+                                            <i class="fa fa-lock" aria-hidden="true"></i>
+                                        </span>
+                                    </div>
+                                </div>
+                                <div class="container-login100-form-btn">
+                                    <button className="registrar100-form-btn" onClick={this.saveUsuario} style={{maxWidth:"1140px"}}><b>Registrar</b></button>
+                                    <button className="volver100-form-btn" onClick={this.cancel} style={{maxWidth:"1140px"}}><b>Volver</b></button>
+                                </div>
+                                </form>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+            </body>
+        )}
+}
+export default AgregarPacienteComponent
--- src/Componentes/Usuario/Modulos/PacienteComponent.js
+++ src/Componentes/Usuario/Modulos/PacienteComponent.js
@@ -2,15 +2,32 @@ import React from 'react';
 import UsuarioService from '../../../Servicios/UsuarioService';
 import HeaderComponent from '../../Complementos/HeaderComponent';
 import Sidebar from '../../Complementos/SidebarComponent';
-import {Table, Button, Container, Modal, ModalBody, ModalHeader ,FormGroup, ModalFooter} from 'reactstrap';
+import { NavLink } from 'react-router-dom';
+import {Table, Button, Modal, ModalBody, ModalHeader ,FormGroup, ModalFooter} from 'reactstrap';
+
+const data = [
+    { id: 1, personaje: "Naruto", anime: "Naruto" },
+    { id: 2, personaje: "Goku", anime: "Dragon Ball" },
+    { id: 3, personaje: "Kenshin Himura", anime: "Rurouni Kenshin" },
+    { id: 4, personaje: "Monkey D. Luffy", anime: "One Piece" },
+    { id: 5, personaje: "Edward Elric", anime: "Fullmetal Alchemist: Brotherhood"},
+    { id: 6, personaje: "Seto Kaiba", anime: "Yu-Gi-Oh!" },
+  ];
 
 class PacienteComponent extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             currentUser: UsuarioService.getCurrentUser(),
+            data: data,
+            modalActualizar: false,
+            modalInsertar: false,
+            form: {
+              id: "",
+              personaje: "",
+              anime: "",
+            },
         }
-        
     }
 
     componentDidMount() {
@@ -19,7 +36,10 @@ class PacienteComponent extends React.Component {
             window.location.reload();
         }
     }
-
+    onlyPaciente (e){
+        localStorage.setItem("paciente", true)
+        localStorage.setItem("isLandingPage", true)
+    }
     render() {
         localStorage.getItem("page")
         return (
@@ -30,7 +50,36 @@ class PacienteComponent extends React.Component {
                     <div className="content-wrapper">
                         <div className="container-modulos">
                             <br/>
-                            <Button color="primary"> Insertar Nuevo Paciente</Button>
+                            <NavLink class="btn btn-primary" to="/pacientes-add" onClick={this.onlyPaciente}> Agregar Paciente</NavLink>
+                            <Table>
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Personaje</th>
+                                        <th>Anime</th>
+                                        <th>Acción</th>
+                                    </tr>
+                                </thead>
+
+                                <tbody>
+                                {this.state.data.map((dato) => (
+                                    <tr key={dato.id}>
+                                    <td>{dato.id}</td>
+                                    <td>{dato.personaje}</td>
+                                    <td>{dato.anime}</td>
+                                    <td>
+                                        <Button
+                                        color="primary"
+                                        /*onClick={() => this.mostrarModalActualizar(dato)}*/
+                                        >
+                                        Editar
+                                        </Button>{" "}
+                                        <Button color="danger" /*onClick={()=> this.eliminar(dato)}*/>Eliminar</Button>
+                                    </td>
+                                    </tr>
+                                ))}
+                                </tbody>
+                            </Table>
                         </div>
                     </div>
                 </div>
